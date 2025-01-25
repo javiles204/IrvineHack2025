78502ddb7da1668af6cb91673076ed07a9520e63
Jason Aviles <jtaviles@uci.edu>
2025-01-25 11:03:08 -0800

    lebron effects

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let gem = document.querySelector('.lebron-aura')
+let aura = document.querySelector('.lebron-aura');
+const lebronHead = document.querySelector('.lebron-image'); 
 
 function incrementAura() {
-    gem.innerHTML = parseFloat(gem.innerHTML) + 1
+    aura.innerHTML = parseFloat(aura.innerHTML) + 1
+
+    const duplicateLebron = document.createElement('img');
+    duplicateLebron.src = './assets/LebronHead.png';
+    duplicateLebron.style.position = 'absolute';
+    duplicateLebron.style.width = '100px';
+
+    const lebronHeadRect = lebronHead.getBoundingClientRect();
+    const randomXOffset = Math.random() * 400 - 100;
+
+    duplicateLebron.style.left = `${lebronHeadRect.left + randomXOffset}px`; 
+    duplicateLebron.style.top = `${lebronHeadRect.top - 300}px`;
+    duplicateLebron.classList.add('drop-lebron');
+
+    document.body.appendChild(duplicateLebron);
+    setTimeout(() => {
+        document.body.removeChild(duplicateLebron); 
+    }, 500) 
 }
